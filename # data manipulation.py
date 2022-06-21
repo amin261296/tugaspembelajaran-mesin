be0cd9e29d6701aa3b43dbf0e231fa8b73162f2c
amin261296 <107931519+amin261296@users.noreply.github.com>
2022-06-21 20:23:52 +0700

    Add files via upload

--- # data manipulation.py
+++ # data manipulation.py
@@ -16,3 +16,10 @@ from sklearn.tree import DecisionTreeClassifier # decision Tree
 drug_data=pd.read_csv('/kaggle/input/drug-classification/drug200.csv')
 drug_data.head()\
 drug_data.shape
+drug_data.info()
+drug_data.describe()
+drug_data.select_dtypes(include='object')
+drug_data['Sex'].value_counts()
+plt.figure(figsize=(5,4))
+sns.countplot(drug_data['Sex'])
+plt.title('counts of observations in Sex')
